599635a3c407d1cd5c023dc17586fb1fd0439b4c
MassiBaldi <massibaldi85@gmail.com>
2018-11-14 17:30:18 +0100

    ok

--- Email/main.js
+++ Email/main.js
@@ -6,10 +6,11 @@ var login = prompt('Scrivi qui la tua mail');
 
 for (var i = 0; i < email.length; i++) {
   console.log(email[i]);
+  console.log(login);
+if (login == email[i]) {
+  document.getElementById('utente').innerHTML = 'Puoi accedere';
 }
-// if (login == email[i]) {
-//   document.getElementById('utente').innerHTML = 'Puoi accedere';
-// }
-// else {
-//   document.getElementById('utente').innerHTML = 'controlla di aver scrtto la tua mail correttamente';
-// }
+else {
+  document.getElementById('utente').innerHTML = 'controlla di aver scrtto la tua mail correttamente';
+}
+}
