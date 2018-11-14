47c385303ffd05523c5782dc3df1a2cc47d33cc2
MassiBaldi <massibaldi85@gmail.com>
2018-11-14 17:04:48 +0100

    email fermo a come scegliere la mail

--- Email/main.js
+++ Email/main.js
@@ -1,9 +1,15 @@
 // lista delle mail accreditate
-var email = ['pippi@gmail.com', 'pluto@gmail.com', 'paperino@gmail.com'];
+var email = ['pippo@gmail.com', 'pluto@gmail.com', 'paperino@gmail.com'];
 console.log(email);
 //chiedo all'utente la sua email
-prompt('Scrivi qui la tua mail');
+var login = prompt('Scrivi qui la tua mail');
 
-if () {
-  document.getElementById('utente').innerHTML = 'Puoi accedere';
+for (var i = 0; i < email.length; i++) {
+  console.log(email[i]);
 }
+// if (login == email[i]) {
+//   document.getElementById('utente').innerHTML = 'Puoi accedere';
+// }
+// else {
+//   document.getElementById('utente').innerHTML = 'controlla di aver scrtto la tua mail correttamente';
+// }
