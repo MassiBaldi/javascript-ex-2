8d81593668aa1cb410346e746ea24fdabf067663
MassiBaldi <massibaldi85@gmail.com>
2018-11-15 10:55:45 +0100

    finito e torna

--- Email/main.js
+++ Email/main.js
@@ -3,14 +3,19 @@ var email = ['pippo@gmail.com', 'pluto@gmail.com', 'paperino@gmail.com'];
 console.log(email);
 //chiedo all'utente la sua email
 var login = prompt('Scrivi qui la tua mail');
+//creo una variabile per gestire i due casi se è stata trovata e se non è stata trovata
+var login_effettuato = false;
 
 for (var i = 0; i < email.length; i++) {
   console.log(email[i]);
   console.log(login);
-if (login == email[i]) {
+  if (login == email[i]){
+    login_effettuato = true;
+  }
+}
+if (login_effettuato == true) {
   document.getElementById('utente').innerHTML = 'Puoi accedere';
 }
 else {
   document.getElementById('utente').innerHTML = 'controlla di aver scrtto la tua mail correttamente';
-}
 }
